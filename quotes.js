edd0c62251662ad4c96e1d8c1a5b8578b2d299c6
Adels <adel20200808@gmail.com>
2025-06-15 18:20:39 +0300

    split app into the JS modules

---
+++ quotes.js
@@ -0,0 +1,64 @@
+const quotes = [
+  {
+    quote: 'The only way to do great work is to love what you do.',
+    author: 'Steve Jobs',
+  },
+  {
+    quote:
+      'Innovation distinguishes between a leader and a follower. - Steve Jobs',
+    author: 'Steve Jobs',
+  },
+  {
+    quote:
+      "Your time is limited, so don't waste it living someone else's life. - Steve Jobs",
+    author: 'Steve Jobs',
+  },
+  {
+    quote:
+      'Success usually comes to those who are too busy to be looking for it.',
+    author: 'Henry David Thoreau',
+  },
+  {
+    quote: 'Don’t be afraid to give up the good to go for the great.',
+    author: 'John D. Rockefeller',
+  },
+  {
+    quote: 'I find that the harder I work, the more luck I seem to have.',
+    author: 'Thomas Jefferson',
+  },
+  {
+    quote:
+      'The future belongs to those who believe in the beauty of their dreams.',
+    author: 'Eleanor Roosevelt',
+  },
+  {
+    quote: 'Dream big and dare to fail.',
+    author: 'Norman Vaughan',
+  },
+  {
+    quote:
+      'You are in danger of living a life so comfortable and soft, that you will die without ever realizing your true potential.',
+    author: 'David Goggins',
+  },
+  {
+    quote:
+      'The most important conversations you’ll ever have are the ones you’ll have with yourself.',
+    author: 'David Goggins',
+  },
+  {
+    quote: 'Suffering is the true test of life.',
+    author: 'David Goggins',
+  },
+  {
+    quote:
+      'Be more than motivated, be more than driven, become literally obsessed to the point where people think you’re fucking nuts.',
+    author: 'David Goggins',
+  },
+  {
+    quote:
+      'You gotta start your journey. It may suck, but eventually you will come out the other side on top.',
+    author: 'David Goggins',
+  },
+];
+
+export default quotes 
