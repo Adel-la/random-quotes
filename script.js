8ae4a2f7ccca6ecf18146786cdd8a6ea8d8e4865
Adels <adel20200808@gmail.com>
2025-06-15 23:17:57 +0300

    no add, commit for connect second user

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-import quotes from "./quotes.js";
+import quotes from './quotes.js';
 const quoteElement = document.getElementById('quote');
 const generateBtn = document.getElementById('generate-btn');
 const authorElement = document.getElementById('author');
+const favoriteBtn = document.getElementById('favorite-quote-btn');
 
+
+  let randomQuote;
 function generateRandomQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomIndex];
-  const{quote, author: quoteAuthor} = randomQuote
+  randomQuote = quotes[randomIndex];
+  const { quote, author: quoteAuthor } = randomQuote;
   quoteElement.textContent = quote;
   authorElement.textContent = quoteAuthor;
+
+  randomQuote.isFavorite = false;
+  favoriteBtn.textContent = '☆';
 }
 
 generateBtn.addEventListener('click', generateRandomQuote);
+
+const arrayOfFavoriteQuotes = [];
+favoriteBtn.addEventListener('click', () => {
+  randomQuote.isFavorite = !randomQuote.isFavorite;
+  favoriteBtn.textContent = randomQuote.isFavorite ? '★' : '☆';
+  arrayOfFavoriteQuotes.push(quoteElement);
+  console.log(randomQuote)
+});
+console.log(arrayOfFavoriteQuotes)
